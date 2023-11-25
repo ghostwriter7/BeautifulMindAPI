31989d4c4c901dcf0d7dc97293165d72127fe424
ghostwriter7 <hubert.rozplochowski@gmail.com>
2023-11-25 18:04:00 +0100

    Initial commit

---
+++ src/main/java/com/beautifulmind/BeautifulMindApiApplication.java
@@ -0,0 +1,13 @@
+package com.beautifulmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulMindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulMindApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beautifulmind/BeautifulMindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautifulmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulMindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
